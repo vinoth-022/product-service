c21ca88673b7e383d24955aac955b96f3c3dd305
VinBotxxii <btraining108@gmail.com>
2024-05-19 07:55:28 +0530

    first commit

--- src/main/java/com/productservice/ProductServiceApplication.java
+++ src/main/java/com/productservice/ProductServiceApplication.java
@@ -2,6 +2,7 @@ package com.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class ProductServiceApplication {
--- src/main/java/com/productservice/service/ProductService.java
+++ src/main/java/com/productservice/service/ProductService.java
@@ -7,6 +7,15 @@ import com.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,16 +28,41 @@ public class ProductService {
 
 
     private final ProductRepository productRepository ;
+    private final WebClient.Builder webClientBuilder; // Inject WebClient Builder
 
+
+    private void decrementProductQuantities(List<String> skuCodes) {
+  	  webClientBuilder.build().post()
+  	    .uri("http://inventory-service/api/inventory/decrement-stock")  // Use the correct endpoint
+  	    .bodyValue(skuCodes)  // Send the entire list in the request body
+  	    .retrieve()
+  	    .toBodilessEntity()
+  	    .block();  
+      }
+    
     public void createProduct(ProductRequest productRequest) {
-        Product product = Product.builder()
+//        String url = "http://inventory-service/api/inventory/find-by-sku/" + productRequest.getName();
+//        System.out.println(url);
+//        boolean isInStock = webClientBuilder.build().get()
+//        	      .uri(url)
+//        	      .retrieve()
+//        	      .bodyToMono(Boolean.class)
+//        	      .block();
+//
+//        	  System.out.println(url);
+//        	  System.out.println("Is in stock: " + isInStock);
+//
+//        	  if (isInStock) {
+    	Product product = Product.builder()
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
                 .build() ;
         productRepository.save(product) ;
-        log.info("Product Created of id {}", product.getId());
-
+//        log.info("Product Created of id {}", product.getId());
+//    	 }else {
+//    		 throw new RuntimeException("Product with name " + productRequest.getName() + " not found in inventory");
+//    	 }
     }
 
     public List<ProductResponse> fetchAllProducts() {
